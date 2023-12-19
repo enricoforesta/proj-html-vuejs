f409f61a7525a6d734f321601e11b85a1fca919e
enricoforesta <enrico.foresta97@gmail.com>
2023-12-19 13:21:54 +0100

    feat:scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script>
+import appHeader from './components/header/appHeader.vue';
+import appMain from './components/main/appMain.vue';
+import appFooter from './components/footer/appFooter.vue';
 export default {
+    components: {
+        appHeader,
+        appMain,
+        appFooter
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <appHeader />
+    <appMain />
+    <appFooter />
+</template>
 
 <style scoped></style>
