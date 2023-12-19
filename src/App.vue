719c67b4bf851a4fcea5b1470cdeb33b9327cd5f
enricoforesta <enrico.foresta97@gmail.com>
2023-12-19 12:54:02 +0100

    feat: scaffolding

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import appHeader from './components/headerComponents/appHeader.vue'
+import appMain from './components/mainComponents/appMain.vue'
+import appFooter from './components/footerComponents/appFooter.vue'
+</script>
+
+<template>
+  <appHeader />
+  <appMain />
+  <appFooter />
+</template>
+
+<style scoped></style>
