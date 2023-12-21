132bf35ba5cbdef5598c2095de2e1f30ba5b9647
enricoforesta <enrico.foresta97@gmail.com>
2023-12-21 10:34:15 +0100

    feat: component List

---
+++ src/components/componentList.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: "list",
+    data() {
+        return {
+            listName: ['home', 'about', 'projects', 'services', 'blog', 'contact',]
+        }
+    }
+}
+</script>
+<template>
+    <ul class="list">
+        <li v-for="item in listName">{{ item.toUpperCase() }}</li>
+    </ul>
+</template>
+<style scoped lang="scss">
+ul.list {
+    display: flex;
+    gap: 15px;
+
+    li {
+        padding-top: 30px;
+        cursor: pointer;
+
+        &:hover {
+            border-top: 2px solid currentColor;
+        }
+    }
+
+}
+</style>
